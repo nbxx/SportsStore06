91536350ebf2254d6f67f5b98395a32bfffa7890
GY <GY>
2020-08-09 16:52:30 -0400

    1 [CHG] add submitting order;

--- SportsStore/Vic.SportsStore.WebApp/Controllers/CartController.cs
+++ SportsStore/Vic.SportsStore.WebApp/Controllers/CartController.cs
@@ -12,10 +12,12 @@ namespace Vic.SportsStore.WebApp.Controllers
     public class CartController : Controller
     {
         private IProductsRepository repository;
+        private IOrderProcessor orderProcessor;
 
-        public CartController(IProductsRepository repo)
+        public CartController(IProductsRepository repo, IOrderProcessor proc)
         {
             repository = repo;
+            orderProcessor = proc;
         }
 
         public ViewResult Index(Cart cart, string returnUrl)
@@ -48,5 +50,36 @@ namespace Vic.SportsStore.WebApp.Controllers
             }
             return RedirectToAction("Index", new { returnUrl });
         }
+
+        public PartialViewResult Summary(Cart cart)
+        {
+            return PartialView(cart);
+        }
+
+        public ViewResult Checkout()
+        {
+            return View(new ShippingDetails());
+        }
+
+        [HttpPost]
+        public ViewResult Checkout(Cart cart, ShippingDetails shippingDetails)
+        {
+            if (cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                orderProcessor.ProcessOrder(cart, shippingDetails);
+                cart.Clear();
+                return View("Completed");
+            }
+            else
+            {
+                return View(shippingDetails);
+            }
+        }
+
     }
 }
